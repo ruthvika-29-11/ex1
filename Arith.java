be4e6c965c24ca2bace07e7700a1d1060bbc348f
ruthvika-29-11 <245122748011@mvsrec.edu.in>
2025-07-22 16:29:01 +0530

    Create Arith.java

---
+++ Arith.java
@@ -0,0 +1,9 @@
+class Arith{
+public static void main(String[] args){
+int a=10;
+int b=20;
+System.out.println("Sum"+(a+b));
+System.out.println("d"+(a-b));
+System.out.println("p"+(a*b));
+System.out.println("d"+(a/b));}
+}
